8f15850fa045cbbef0b3a08ba1366cb71e939602
Abhijeet Singh Rajput <abhijeet62008@gmail.com>
2024-11-28 21:06:25 +0530

    added: Longest Substring Without Repeating Length

--- termwork_PMC-103/ps7.c
+++ termwork_PMC-103/ps7.c
@@ -1,5 +1,26 @@
 #include<stdio.h>
+#include<string.h>
+
 int main(){
-    
+    int lastIndex[256];
+    for(int i = 0; i<256; ++i){
+        lastIndex[i] = -1;
+    }
+
+    char *str = "xyzwwepppw";
+    int maxLen = 0;
+    int i = 0;
+    for(int j = 0; j<strlen(str); ++j){
+        if(lastIndex[str[j]] >= i){
+            i = lastIndex[str[j]] + 1;
+        }
+        lastIndex[str[j]] = j;
+        int len = j - i + 1;
+        if(len > maxLen){
+            maxLen = len;
+        }
+    }
+
+    printf("Longest Substring Without Repeating Length: %d", maxLen);
     return 0;
 }
